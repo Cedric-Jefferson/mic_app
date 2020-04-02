cb2e0bfa2ec0878442c37d9fe53daaacf52d631d
Cedric-Jefferson <46280843+Cedric-Jefferson@users.noreply.github.com>
2020-04-02 12:33:34 +0200

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import ReactMic from './ReactMic';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  //const { getByText } = render(<App />);
+  const { getByText } = render(<ReactMic />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactMic from './ReactMic';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReactMic />
   </React.StrictMode>,
   document.getElementById('root')
 );
